8ce9a41bb5ec3b109fca6599bcf600abdb52f0a4
vipul <vipul.sharma20@gmail.com>
2015-08-24 02:46:45 +0530

    initial code for bot added

---
+++ bot.py
@@ -0,0 +1,35 @@
+import socket
+
+server = "irc.freenode.net"
+channel = "#jec-dev"
+nick = "Mybot"
+
+def ping(): # responds to server pings
+  ircsock.send("PONG :pingis\n")
+
+def sendmsg(chan , msg):
+  ircsock.send("PRIVMSG "+ chan +" :"+ msg +"\n")
+
+def joinchan(chan):
+  ircsock.send("JOIN "+ chan +"\n")
+
+def hello():
+  ircsock.send("PRIVMSG "+ channel +" :Hello!\n")
+
+ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ircsock.connect((server, 6667)) # Here we connect to the server using the port 6667
+ircsock.send("USER "+ botnick +" "+ botnick +" "+ botnick +" Test Bot\n") # user authentication
+ircsock.send("NICK "+ botnick +"\n") # here we actually assign the nick to the bot
+
+joinchan(channel)
+
+while 1:
+  ircmsg = ircsock.recv(2048)
+  ircmsg = ircmsg.strip('\n\r') # removing any unnecessary linebreaks.
+  print(ircmsg) # server messages
+
+  if ircmsg.find(":Hello "+ botnick) != -1:
+    hello()
+
+  if ircmsg.find("PING :") != -1:
+    ping()
