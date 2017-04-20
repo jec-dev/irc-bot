2847caf6c063c55fbab921f54f211a3882092027
Vipul <vipul.sharma20@gmail.com>
2017-04-20 13:11:19 +0530

    pep8 compliant

--- bot.py
+++ bot.py
@@ -4,33 +4,41 @@ server = "irc.freenode.net"
 channel = "#jec-dev"
 botnick = "Mybot"
 
-def ping(): # responds to server pings
-  ircsock.send("PONG :pingis\n")
 
-def sendmsg(chan , msg):
-  ircsock.send("PRIVMSG "+ chan +" :"+ msg +"\n")
+def ping():  # responds to server pings
+    ircsock.send("PONG :pingis\n")
+
+
+def sendmsg(chan, msg):
+    ircsock.send("PRIVMSG " + chan + " :" + msg + "\n")
+
 
 def joinchan(chan):
-  ircsock.send("JOIN "+ chan +"\n")
+    ircsock.send("JOIN " + chan + "\n")
+
 
 def hello():
-  ircsock.send("PRIVMSG "+ channel +" :Hello! Welcome to jec-dev! Happy \
+    ircsock.send("PRIVMSG " + channel + " :Hello! Welcome to jec-dev! Happy \
           Hacking! :D\n")
 
+
 ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ircsock.connect((server, 6667)) # Here we connect to the server using the port 6667
-ircsock.send("USER "+ botnick +" "+ botnick +" "+ botnick +" Test Bot\n") # user authentication
-ircsock.send("NICK "+ botnick +"\n") # here we actually assign the nick to the bot
+# Here we connect to the server using the port 6667
+ircsock.connect((server, 6667))
+ircsock.send("USER " + botnick + " " + botnick + " " +
+             botnick + " Test Bot\n")  # user authentication
+# here we actually assign the nick to the bot
+ircsock.send("NICK " + botnick + "\n")
 
 joinchan(channel)
 
 while 1:
-  ircmsg = ircsock.recv(2048)
-  ircmsg = ircmsg.strip('\n\r') # removing any unnecessary linebreaks.
-  print(ircmsg) # server messages
+    ircmsg = ircsock.recv(2048)
+    ircmsg = ircmsg.strip('\n\r')  # removing any unnecessary linebreaks.
+    print(ircmsg)  # server messages
 
-  if ircmsg.find(":Hello "+ botnick) != -1:
-    hello()
+    if ircmsg.find(":Hello " + botnick) != -1:
+        hello()
 
-  if ircmsg.find(":PING ") != -1:
-    ping()
+    if ircmsg.find(":PING ") != -1:
+        ping()
